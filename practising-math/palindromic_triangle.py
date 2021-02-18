b713761d25368033bd66b2b6b9abc137b4a74e21
Felice Sansonetti <felice.sansonetti@gmail.com>
2021-02-18 17:14:52 +0100

    eliminating second for loop

--- practising-math/palindromic_triangle.py
+++ practising-math/palindromic_triangle.py
@@ -5,8 +5,7 @@
 def main():
 
     for n in range(1, int(input())+1):
-        xx = list(range(1, n)) + list(range(n, 0, -1))
-        print(sum([xx[l] * (10 ** (len(xx) - 1 - l)) for l in range(0, len(xx))]))
+        print(int(((10**n-1)/9)**2))
 
 if __name__ == "__main__":
 	main()
